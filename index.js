e83ac32d1d4f23377a4e4495ac9c8601c1843a80
fredericocurti <frederico.vilelacurti@gmail.com>
2021-02-19 15:28:44 -0300

    added lambda chromium and puppeteer core

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-const puppeteer = require('puppeteer');
+const chromium = require('chrome-aws-lambda');
 var Promise = require('bluebird');
 const hb = require('handlebars')
+const chromium = require('chrome-aws-lambda');
 
 module.exports
 async function generatePdf(file, options, callback) {
@@ -14,8 +15,12 @@ async function generatePdf(file, options, callback) {
     delete options.args;
   }
 
-  const browser = await puppeteer.launch({
-    args: args
+  browser = await chromium.puppeteer.launch({
+    args: [chromium.args, ...args],
+    defaultViewport: chromium.defaultViewport,
+    executablePath: await chromium.executablePath,
+    headless: chromium.headless,
+    ignoreHTTPSErrors: true,
   });
   const page = await browser.newPage();
 
@@ -42,47 +47,4 @@ async function generatePdf(file, options, callback) {
     }).asCallback(callback);
 }
 
-async function generatePdfs(files, options, callback) {
-  // we are using headless mode
-  let args = [
-    '--no-sandbox',
-    '--disable-setuid-sandbox',
-  ];
-  if(options.args) {
-    args = options.args;
-    delete options.args;
-  }
-  const browser = await puppeteer.launch({
-    args: args
-  });
-  let pdfs = [];
-  const page = await browser.newPage();
-  for(let file of files) {
-    if(file.content) {
-      console.log("Compiling the template with handlebars")
-      // we have compile our code with handlebars
-      const template = hb.compile(file.content, { strict: true });
-      const result = template(file.content);
-      const html = result;
-      // We set the page content as the generated html by handlebars
-      await page.setContent(html);
-    } else {
-      await page.goto(file.url, {
-        waitUntil: 'networkidle0', // wait for page to load completely
-      });
-    }
-    let pdfObj = JSON.parse(JSON.stringify(file));
-    delete pdfObj['content'];
-    pdfObj['buffer'] = Buffer.from(Object.values(await page.pdf(options)));
-    pdfs.push(pdfObj);
-  }
-
-  return Promise.resolve(pdfs)
-    .then(async function(data) {
-       await browser.close();
-       return data;
-    }).asCallback(callback);
-}
-
 module.exports.generatePdf = generatePdf;
-module.exports.generatePdfs = generatePdfs;
